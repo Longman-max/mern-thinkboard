a133af62a8c6711e39b5acfb81ee1e1b8ec7c087
Obasi Agbai <obasiagbai94@gmail.com>
2025-08-14 05:37:44 +0100

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,47 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
+import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import notesRoutes from "./routes/noteRoutes.js";
 
-// accessing environment variables
 dotenv.config();
 
-// initializing express app
 const app = express();
-
-// accessing env variables
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    orign: "http://localhost:5173",
-  })
-);
-app.use(express.json()); // this middleware will parse the JSON bodies: req.body
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// simple custom middleware
+// our simple custom middleware
 // app.use((req, res, next) => {
 //   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //   next();
 // });
 
-// routing
 app.use("/api/notes", notesRoutes);
 
-// connecting to the database and listen in a port
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
-  // listening to the server
   app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`);
+    console.log("Server started on PORT:", PORT);
   });
 });
